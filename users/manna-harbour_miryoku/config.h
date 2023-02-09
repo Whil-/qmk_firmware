036119538931be66ff2911d95273f9f6a02d8554
Gustav <gustav@whil.se>
2023-02-09 08:30:04 +0100

    Changes imported 2023-02-09
    From: miryoku_qmk and miryoku_babel
    
    miryoku_qmk didn't build. That's why I'm doing this.

--- users/manna-harbour_miryoku/config.h
+++ users/manna-harbour_miryoku/config.h
@@ -14,6 +14,9 @@
 // Prevent normal rollover on alphas from accidentally triggering mods.
 #define IGNORE_MOD_TAP_INTERRUPT
 
+// Mostly for shift to be able to trigger faster than the tapping term.
+#define PERMISSIVE_HOLD
+
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define TAPPING_FORCE_HOLD
 
@@ -40,4 +43,3 @@
   #define COMBO_TERM 200
   #define EXTRA_SHORT_COMBOS
 #endif
-
