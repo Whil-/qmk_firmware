ddf5d631cb575c8469c9801a287f9be08a868292
Gustav <gustav@whil.se>
2023-11-12 00:34:32 +0100

    Custom version of crkbd

---
+++ keyboards/crkbd/keymaps/whil/rules.mk
@@ -0,0 +1,14 @@
+# markstos: enable media keys
+EXTRAKEY_ENABLE = yes
+
+# markstos: smaller file size, little down-side
+LTO_ENABLE = yes
+
+# This is for RGB *underglow*
+# https://github.com/qmk/qmk_firmware/blob/master/docs/feature_rgblight.md
+RGBLIGHT_ENABLE = yes
+
+# https://github.com/samhocevar-forks/qmk-firmware/blob/master/docs/feature_rgb_matrix.md
+# RGB_MATRIX_ENABLE = WS2812
+
+OLED_DRIVER_ENABLED = yes
