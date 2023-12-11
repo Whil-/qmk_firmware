7293418c5a60d2ed4778469edd4094ab3832bc29
Gustav <gustav@whil.se>
2023-12-11 20:15:35 +0100

    Move QK_BOOT key away from mouse scroll up down

--- keyboards/crkbd/keymaps/whil/keymap.c
+++ keyboards/crkbd/keymaps/whil/keymap.c
@@ -127,7 +127,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       KC_CAPS,  KC_F11,  KC_F12, KC_BTN2, KC_BTN1, KC_BTN3,                       KC_APP, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R,BSE_SMAC,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,                      XXXXXXX, XXXXXXX, KC_WH_D, KC_WH_U, QK_BOOT, KC_RSFT,
+      RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, RGB_SPI,                      QK_BOOT, XXXXXXX, KC_WH_D, KC_WH_U, XXXXXXX, KC_RSFT,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
                                           XXXXXXX, _______, XXXXXXX,    KC_RSFT, _______, XXXXXXX
                                       //`--------------------------'  `--------------------------'
